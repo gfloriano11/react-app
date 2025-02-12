ebaee0071719c17d57b51b5d2fae5110d1992918
gfloriano11 <gflorianodev@gmail.com>
2025-02-12 19:56:31 -0300

    feat: start to use props in tasks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 function App(){
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    title: 'Estudar base React JS',
+    description: 'Estudo para aprender a mexer com react js e seus fundamentos',
+    isComplete: false
+  },
+  {
+    id: 2,
+    title: 'Estudar base TailwindCSS',
+    description: 'Estudo para aprender a mexer com TailwindCSS e seus fundamentos junto com react',
+    isComplete: false
+  },
+  {
+    id: 3,
+    title: 'Projeto com React JS e TailwindCSS',
+    description: 'Projeto próprio para por em prática os estudos de react js com o tailwind.',
+    isComplete: false
+  }
+])
+
   return (
-    <div>
-      <h1>Tasks Management</h1>
-      <Tasks/>
-      <AddTask/>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Tasks Management</h1>
+        <Tasks tasks={tasks}/>
+        <AddTask/>
+      </div>
     </div>
   );
 }
