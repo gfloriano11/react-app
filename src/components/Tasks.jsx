22fcedef2fcb9707afd2c9b735bfa335086b217d
gfloriano11 <gflorianodev@gmail.com>
2025-02-12 20:30:17 -0300

    feat: create design to tasks in app

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,15 @@
+import { ChevronRightIcon } from "lucide-react";
+
 function Tasks(props){
     return (
         <div>
-            <ul className="space-y-3">
+            <ul className="space-y-3 p-6 bg-slate-200 rounded-xl shadow">
                 {props.tasks.map((task) => (
-                    <li key={task.div} className="bg-slate-400 text-white p-2 rounded-xl">
-                        {task.title}
+                    <li key={task.id} className="flex gap-1">
+                        <button className="bg-slate-400 w-full text-white p-2 rounded-xl text-left">{task.title}</button>
+                        <button className="bg-slate-400 text-white p-2 rounded-xl">
+                          <ChevronRightIcon/>
+                        </button>
                     </li>
                 ))}
             </ul>
