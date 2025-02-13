001da13e2c465db3c3188c48c2a0f5afe2313896
gfloriano11 <gflorianodev@gmail.com>
2025-02-13 19:39:24 -0300

    feat: add container to add task

--- src/App.jsx
+++ src/App.jsx
@@ -42,10 +42,10 @@ function App(){
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Tasks Management</h1>
-        <Tasks tasks={tasks} clickTask={clickTask} deleteTask={deleteTask}/>
         <AddTask/>
+        <Tasks tasks={tasks} clickTask={clickTask} deleteTask={deleteTask}/>
       </div>
     </div>
   );
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,9 @@
 function AddTask(){
     return (
-        <div>
-            <h2>Add Task</h2>
+        <div className="space-y-3 p-6 bg-slate-200 rounded-xl shadow flex flex-col">
+            <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task name"></input>
+            <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task description"></input>
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Add Task</button>
         </div>
     );
 }
