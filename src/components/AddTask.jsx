1b6938b3c1afa776c2a09b7eeedc4f5e024eff1e
gfloriano11 <gflorianodev@gmail.com>
2025-02-15 11:15:41 -0300

    feat: add trim to remove black spaces

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -10,7 +10,7 @@ function AddTask({submitTask}){
             <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task name" value={title} onChange={(event) => setTitle(event.target.value)}></input>
             <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task description" value={desc} onChange={(event) => setDesc(event.target.value)}></input>
             <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium" onClick={() => {
-                if(!(title === '')  ){
+                if(!(title.trim() === '')  ){
                     submitTask(title, desc)
                     setTitle('');
                     setDesc('');
