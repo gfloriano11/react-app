9e671dcb414d1163d37241ac0454ba87e91f05aa
gfloriano11 <gflorianodev@gmail.com>
2025-02-15 11:00:16 -0300

    feat: verification if task name is empt

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -9,8 +9,13 @@ function AddTask({submitTask}){
         <div className="space-y-3 p-6 bg-slate-200 rounded-xl shadow flex flex-col">
             <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task name" value={title} onChange={(event) => setTitle(event.target.value)}></input>
             <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task description" value={desc} onChange={(event) => setDesc(event.target.value)}></input>
-            <button onClick={() => submitTask(title, desc)} className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
-                Add Task
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium" onClick={() => {
+                if(!(title === '')  ){
+                    submitTask(title, desc)
+                    setTitle('');
+                    setDesc('');
+                }
+            }}> Add Task
             </button>
         </div>
     );
