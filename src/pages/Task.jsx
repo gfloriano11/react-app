7a55f28eb815cebb24ebe2216dbfe4145140c72e
gfloriano11 <gflorianodev@gmail.com>
2025-02-22 11:32:23 -0300

    feat: get task info by query params

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,7 +1,13 @@
+import { useSearchParams } from "react-router-dom";
+
 function Task(){
+    const [searchParams] = useSearchParams();
+    const title = searchParams.get('title');
+    const description = searchParams.get('description')
     return(
-        <div>
-            <h1>Task</h1>
+        <div className="h-screen w-screen bg-slate-500 p-6">
+            <h1>{title}</h1>
+            <h1>{description}</h1>
         </div>
     );
 }
