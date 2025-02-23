ed71611d2caf1404942011c87f45f3c90b181c80
gfloriano11 <gflorianodev@gmail.com>
2025-02-23 17:12:57 -0300

    feat: create input component to dont rewrite several times

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,3 +1,4 @@
+import Input from "./Input";
 import { useState } from "react";
 
 function AddTask({submitTask}){
@@ -7,8 +8,8 @@ function AddTask({submitTask}){
 
     return (
         <div className="space-y-3 p-6 bg-slate-200 rounded-xl shadow flex flex-col">
-            <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task name" value={title} onChange={(event) => setTitle(event.target.value)}></input>
-            <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" type="text" placeholder="Task description" value={desc} onChange={(event) => setDesc(event.target.value)}></input>
+            <Input type="text" placeholder="Task name" value={title} onChange={(event) => setTitle(event.target.value)}/>
+            <Input type="text" placeholder="Task description" value={desc} onChange={(event) => setDesc(event.target.value)}/>
             <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium" onClick={() => {
                 if(!(title.trim() === '')  ){
                     submitTask(title, desc)
