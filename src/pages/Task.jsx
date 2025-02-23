854cbb7e329fe46465a2bf2a78ef50d8f40b9fb8
gfloriano11 <gflorianodev@gmail.com>
2025-02-23 10:32:18 -0300

    feat: task title and description

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -6,9 +6,20 @@ function Task(){
     const description = searchParams.get('description')
     return(
         <div className="h-screen w-screen bg-slate-500 p-6">
-            <h1>{title}</h1>
-            <h1>{description}</h1>
+            {/* <h1>{title}</h1>
+            <h1>{description}</h1> */}
+            <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+                <div className="w-[500px] space-y-4">
+                    <h1 className="text-3xl text-slate-100 font-bold text-center">Task Details</h1>
+
+                    <div className="bg-slate-400 p-4 rounded-md">
+                        <h2 className="text-xl text-white font-bold">{title}</h2>
+                        <p className="text-white">{description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     );
 }
 
