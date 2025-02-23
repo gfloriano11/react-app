f5e98f047ad7e122c728d4aff83a95c2be0e8560
gfloriano11 <gflorianodev@gmail.com>
2025-02-23 15:49:38 -0300

    feat: task page can go back to menu

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,21 +1,25 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Task(){
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const title = searchParams.get('title');
-    const description = searchParams.get('description')
+    const description = searchParams.get('description');
     return(
-        <div className="h-screen w-screen bg-slate-500 p-6">
-            {/* <h1>{title}</h1>
-            <h1>{description}</h1> */}
-            <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-                <div className="w-[500px] space-y-4">
+        <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+            <div className="w-[500px] space-y-4">
+                <div className="flex justify-center relative mb-6">
+                    <button
+                        onClick={() => navigate(-1)} 
+                        className="bg-slate-200 rounded-md absolute left-0 top-0 bottom-0 text-slate-700">
+                        <ChevronLeftIcon></ChevronLeftIcon>
+                    </button>
                     <h1 className="text-3xl text-slate-100 font-bold text-center">Task Details</h1>
-
-                    <div className="bg-slate-200 p-4 rounded-md">
-                        <h2 className="text-xl font-bold text-slate-600">{title}</h2>
-                        <p className="text-slate-600">{description}</p>
-                    </div>
+                </div>
+                <div className="bg-slate-200 p-4 rounded-md">
+                    <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+                    <p className="text-slate-600">{description}</p>
                 </div>
             </div>
         </div>
